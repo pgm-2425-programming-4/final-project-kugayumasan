91960d5fe690e05de6d8ad34762e705e43e5e2a8
kugayumasan <erosferri@hotmail.com>
2025-05-22 11:08:40 +0200

    created constants variable

---
+++ client/src/constants/constants.js
@@ -0,0 +1,12 @@
+export const API_URL = import.meta.env.PROD
+
+  ? "https://final-project-kugayumasan.onrender.com"
+
+  : "http://localhost:1337/api";
+
+export const API_TOKEN = import.meta.env.PROD
+
+  ? "PROD token"
+
+  : "bab711979994805445c453999adfd618e07f89ba4490ad344d142446e956693440e5bdb01732d0392be9be9ae23a9f34d1b2778acb1460a10c855d548cb3337180ce6ab34b52f97964d68f22ee40ba42c0c9c2b9e08e35d641f5767781bcf28df95a6def6872496c4881bc415d37d125708b3c6bd5f93f748a9fb8cae6f825b7";
+ 
